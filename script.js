aed6d626d9799671dba3b420e6f3377887f85cc0
Silas Abayomi <abayomisilas50@gmail.com>
2024-02-26 17:56:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let button = document.querySelector('button');
+let inputField = document.querySelector('p');
+
+let url = 'https://www.boredapi.com/api/activity';
+// let url = 'https://catfact.ninja/fact';
+async function callThisApi () {
+       event.preventDefault()
+        
+
+       try {
+             const request = await fetch(url);
+             let response  = await request.text(); 
+
+
+              let rest = response.substring(0, response.indexOf('type')).replace(/["{}]/g, '').replace('activity:', 'Activity: ');
+
+
+             inputField.innerHTML = rest;
+       
+             return response
+       } catch (error) {
+              console.log('So sorry we just called the api wrongly, please check', error);   
+       }
+}
+
+
+callThisApi();
+
+
+function goToHomePage() {
+       // Change the URL to the desired next page
+       window.location.href = "home.html";
+     }
